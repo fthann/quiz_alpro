3f90357b41d99e9da381d05619244ab2269b6ae5
fthann <fathanalfrq@gmail.com>
2024-12-08 07:45:32 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukan angka pertama: ");
+        double angkaPertama = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Masukan angka kedua: ");
+        double angkaKedua = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Masukan angka ketiga: ");
+        double angkaKetiga = Convert.ToDouble(Console.ReadLine());
+
+        double rataRata = (angkaPertama + angkaKedua + angkaKetiga) / 3;
+
+        Console.WriteLine("Rata-rata nya ialah: " + rataRata);
+    }
+}
